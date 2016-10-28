b72659cd686e57c7ad2cbf82f5ef1f5ffc5582bc
LeheneFlaviu <flaviu.lehene@gmail.com>
2016-10-28 17:12:14 +0300

    First Commit for the project

---
+++ Wordpress/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace Wordpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello world!");
+             Console.WriteLine("Press any key to stop...");
+             Console.ReadKey();*/
+
+            WordpressPage page = new WordpressPage("titlu_aaa","content_aaa sdags sdsgg");
+            WordpressPage SecondPage = new WordpressPage("Al doilea", "si al 2-lea");           
+            //Console.WriteLine(page);
+            //Console.WriteLine();
+           // Console.WriteLine(SecondPage);
+            //Console.ReadKey();
+
+
+
+            WordpressPage thirdPage = new WordpressPage("third", "blabla3");
+            WordpressPage fourthPage = new WordpressPage("patru", "dfssdffdssfd");
+
+            //thirdPage.status = WordpressPage.Status.PendingReview;
+            //fourthPage.viewCount = 999999;
+
+            //Console.WriteLine(thirdPage);
+            //Console.WriteLine();
+            //Console.WriteLine(fourthPage);
+            //Console.ReadKey();
+
+            //page.Publish();
+            
+            //Console.WriteLine(page);
+            //Console.ReadKey();
+
+            //page.PendingReview();
+            //SecondPage.PendingReview();
+            //foreach (var i in WordpressPage.lista)
+            //{
+            //    Console.WriteLine(i);
+            //    Console.ReadKey();
+            //}
+
+            //thirdPage.Publish(5);
+            //fourthPage.Publish(DateTime.Now.AddDays(1));
+            //thirdPage.SetChild(SecondPage);
+            //fourthPage.SetParent(page);
+
+            //Console.WriteLine(page.GetWordCount());
+            //Console.WriteLine(thirdPage);
+            //Console.WriteLine(SecondPage);
+            //Console.WriteLine(fourthPage);
+            Console.WriteLine(page.isPublished);
+            page.status = WordpressPage.Status.Published;
+            try
+            {
+                page.Publish();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(page.isPublished);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Wordpress/WordpressPage.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpress
+{
+    public class WordpressPage
+    {
+        public int id = 99;
+        public string title = "blabla";
+        public string content = "abcdefg";
+        public enum Status
+        {
+            Draft,
+            Published,
+            PendingReview
+        };
+        public Status status  { get; set ; } = Status.PendingReview;
+        public Boolean isVisible = true;
+        public DateTime publishDate;        
+        public int parent =1;
+        public int order = 2;
+        public int viewCount = 3;
+        public static int pageCount = 0;
+        public static List<WordpressPage> lista = new List<WordpressPage>();
+        public bool isPublished
+        {
+            get
+            {             
+                return (this.status == Status.Published);
+            }
+        }
+       
+        public WordpressPage(string title, string content)
+        {
+            this.title = title;
+            this.content = content;
+            this.id = ++pageCount;
+        }
+
+        public void Publish()
+        {
+                if (this.isPublished)
+                {
+                    throw new Exception("Pagina este deja publicata!");                    
+                }
+            
+            this.publishDate = DateTime.Now;
+            this.status = Status.Published;
+
+        }
+
+        public void Publish(int hours)
+        {
+
+                if (this.isPublished)
+                {
+                    throw new Exception();
+                }
+
+            this.publishDate = DateTime.Now.AddHours(hours);
+            this.status = Status.Published;
+        }
+
+        public void Publish(DateTime dateTime)
+        {
+
+                if (this.isPublished)
+                {
+                    throw new Exception();
+                }
+
+            this.publishDate = dateTime;
+            this.status = Status.Published;
+        }
+
+        public void SetChild(WordpressPage page) //instanta curenta primeste copil page == page primeste parinte instanta curenta
+        {
+            page.parent = this.id;
+        }
+        public void SetParent(WordpressPage page)
+        {
+            this.parent = page.id;
+        }
+
+        public int GetWordCount()
+        {
+            string [] split = this.content.Split(' ');
+            return split.Length;
+        }
+
+        public void PendingReview()
+        {        
+            this.status = Status.PendingReview;
+            lista.Add(this);
+        }
+
+        public override string ToString()
+        {
+
+            return "ID: " + id + "\nTitle: " + title + "\nContent: " + content + "\nStatus:" + status + "\nisVisible: " 
+                + isVisible + "\npublishdate: " + publishDate + "\nParent: " + parent + "\nOrder: " + order + "\nviewCount: " + viewCount + "\n";
+            //return Convert.ToString(id);
+            
+        }
+    }
+}
