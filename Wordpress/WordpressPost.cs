bee5a466df237dd239d0be2fca6305b2d296e2e3
LeheneFlaviu <flaviu.lehene@gmail.com>
2016-10-29 14:51:33 +0300

    WordpressPost commit

---
+++ Wordpress/WordpressPost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpress
+{
+    public class WordpressPost : WordpressPage
+    {
+        public enum Format
+        {
+            Standard,
+            Aside,
+            Image,
+            Video,
+            Quote,
+            Link,
+            Gallery,
+            Status,
+            Audio,
+            Chat
+        };
+        public Format format;
+        public List<string> Categories;
+        public List<string> Tags;
+        string featuredImage;
+
+        public WordpressPost(string title, string content) : base(title, content)
+        {
+
+        }
+    }
+}
+
