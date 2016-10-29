447040b14203d42bb8e41603b4b9362668be9d8c
ovidiub <ovidiub@TOSHIBASATPRO>
2016-10-29 14:50:26 +0300

    post

---
+++ Wordpress/WordpressPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpress
+{
+    class WordpressPost : WordpressPage
+    {
+        List<string> categories = new List<string>();
+        List<string> tags = new List<string>();
+
+
+        enum Formats = {Standard, Aside, Image, Video, Quote, Link };
+        Format format
+        public WordpressPost(string title, string content) : base(title, content)
+        {
+        }
+    }
+}
