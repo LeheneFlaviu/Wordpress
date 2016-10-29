4a5b28254a6c85573c0fd0b9dd746f9402180e24
dianagogoloiu <GDts1903?>
2016-10-29 14:51:13 +0300

    added a new class for Post

---
+++ Wordpress/WordpressPost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordpress
+{
+    class WordpressPost:WordpressPage
+    {
+        public enum Format
+        {   Standard,
+            Aside,
+            Image,
+            Video,
+            Quote,
+            Link,
+            Gallery,
+            Status,
+            Audio,
+            Chat
+        };
+        public Format format;
+        public List<String> Categories;
+        public List<String> Tags;
+        string featuredImage;
+
+
+
+
+    }
+}
